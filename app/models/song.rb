5feb9370eb70ec3a50b36d9f4a0eccf47927f719
Rico Mitchell <ricomitch@icloud.com>
2020-04-14 17:25:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
-
+  belongs_to :artists
+  #this tells our song class that it will produce objects
+  #that belong to artist
+  belongs_to :genre
+  #our song class also belongs to genre
 end
